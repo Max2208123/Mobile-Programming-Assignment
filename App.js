a72bf86bfd6eaeda5d8e734951b0832886dd5da3
Max2208123 <mscherer25@students.oamk.fi>
2026-02-15 14:08:43 +0200

    Login Page Visual

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
 import FooterMenu from './components/FooterMenu';
 import { useEffect , useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
@@ -12,36 +12,71 @@ import LocationListPage from './pages/LocationListPage';
 import CountriesPage from './pages/CountriesPage';
 
 
+
+
+
+
 const menuPages = [
-  {
-    name: 'Map',
-    component: MapPage,
-    iconName: 'location-sharp'
-  },
-  {
-    name: 'Locations',
-    component: LocationListPage,
-    iconName: 'list'
-  },
-  {
-    name: 'Countries',
-    component: CountriesPage,
-    iconName: 'flag'
-  }
+  { name: 'Map', component: MapPage, iconName: 'location-sharp' },
+  { name: 'Locations', component: LocationListPage, iconName: 'list'},
+  { name: 'Countries', component: CountriesPage, iconName: 'flag' }
 ];
 
 
-
 export default function App() {
+   
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [loggedInUser, setLoggedInUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  
+
+  const checkLoggedIn = async () => {
+    try {
+      const value = await AsyncStorage.getItem('logged-in');
+      if (value === 'true'){
+        setLoggedIn(true);
+        setLoggedInUser(await AsyncStorage.getItem('logged-in-user'))
+      } else {
+        setLoggedIn(false);
+        setLoggedInUser(null);
+      }
+    } catch (e) {
+      console.log("Reading Error:", e);
+      setLoggedIn(false);
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    checkLoggedIn();
+  }, [] );
+ 
+  console.log(loggedIn)
   return(
-    <NavigationContainer>
-      <FooterMenu 
-        pages = {menuPages} 
-        colorActive = 'lightblue' 
-        colorInactive= 'gray' 
-      />
-    </NavigationContainer>
-  )
+    isLoading ? 
+    <View style= {styles.container}>
+      <ActivityIndicator size="large" color = "lightblue"/>
+    </View> : (
+      loggedIn 
+      ? 
+      <NavigationContainer>
+        <View style = {styles.container}>
+          <FooterMenu 
+            pages = {menuPages} 
+            colorActive = 'lightblue' 
+            colorInactive= 'gray' 
+          />
+        </View>
+        
+      </NavigationContainer>
+      : (
+      <View style = {styles.containerLogin}>
+        <LoginPage/>
+      </View>
+      )
+    )
+  );
 }
 
 const styles = StyleSheet.create({
@@ -51,4 +86,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-})
+  containerLogin: {
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100%',
+    backgroundColor: '#AEF3E7',
+  }
+}) 
--- pages/LoginPage.js
+++ pages/LoginPage.js
@@ -1,10 +1,122 @@
-import { View } from 'react-native';
+import React, {useState} from 'react';
+import { View , Text, TextInput, StyleSheet, Button, Pressable } from 'react-native';
 
 export default function LoginPage(){
+    const [usernameInput, setUsernameInput] = useState("");
+    const [passwordInput, setPasswordInput] = useState("");
+
+    const [errorMessageUsername, setErrorMessageUsername] = useState("");
+    const [errorMessagePassword, setErrorMessagePassword] = useState("");
 
     return(
-        <View>
-            <Text> Login Page </Text>
+        <View style = {styles.containerOuter}>
+            <View style = {styles.containerInner}>
+                <Text style= {styles.title1}> Login Page </Text>
+                <View style = {styles.containerInputFields}>
+                    <View style = {styles.containerInputField}>
+                        <TextInput 
+                            onChangeText={setUsernameInput}
+                            value = {usernameInput}
+                            placeholder="Enter Username"
+                            style = {styles.textInputField}
+                        />
+                        <Text style= {styles.errorText}>{errorMessageUsername}</Text>
+                    </View>
+                    <View style = {styles.containerInputField}>
+                        <TextInput 
+                            onChangeText={setPasswordInput}
+                            value = {passwordInput}
+                            placeholder="Enter Password"
+                            style = {styles.textInputField}
+                        />  
+                        <Text style= {styles.errorText}>{errorMessagePassword}</Text>
+                    </View> 
+                </View>
+                <View style = {styles.containerButtons}>
+                    <View style = {styles.buttonContainer}>
+                        <Pressable style = {styles.pressable}>
+                            <Text style = {styles.pressableText}>Register</Text>
+                        </Pressable>
+                    </View>
+                    <View style = {styles.buttonContainer}>
+                        <Pressable style = {styles.pressable}>
+                            <Text style = {styles.pressableText}>Login</Text>
+                        </Pressable>
+                    </View>                   
+                </View>
+            </View>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    textInputField: {
+        borderWidth: 1,
+        fontSize: 24,
+        borderRadius: 48,
+        borderColor: '#254E70',
+        color: '#254E70',
+        paddingLeft: 20,
+    },
+    containerInner: {
+        borderColor: '#254E70',
+        width: '100%',
+        height: '100%',
+        borderWidth: 1,
+        justifyContent:'center',
+        padding: 10,        
+    },
+    containerOuter: {        
+        width : '80%',
+        height: '40%',
+        backgroundColor: '#8EE3EF',
+        alignItems:'center',
+        justifyContent: 'center',
+        padding: 10,
+        borderRadius: 10,
+    },
+    title1: {
+        fontSize: 32,
+        marginBottom: 10,
+        marginTop: 20,
+    },
+    errorText:{
+        color: '#C33C54',
+        paddingLeft: '8%',
+        paddingRight: '8%',
+        fontWeight:'bold'
+    },
+    containerInputField:{
+        marginTop: 5
+    },
+    containerInputFields:{
+        marginTop: 10,
+        marginBottom: 0,
+    },
+
+
+
+    buttonContainer:{
+        width: '40%',
+        marginRight: '5%',   
+        marginLeft:'5%' ,
+        marginTop: '5%',
+    },
+    containerButtons:{
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'end',
+    },
+    pressable:{
+        backgroundColor: '#37718E',
+        borderWidth: 1,
+        height: 54,
+        borderRadius: 27,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    pressableText:{
+        color: '#AEF3E7',
+        fontSize: 24,
+    }
+})
