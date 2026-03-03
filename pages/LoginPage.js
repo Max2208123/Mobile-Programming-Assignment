7830b4d7f08f0b81e5023f70f9c3957e93968b9a
Max2208123 <mscherer25@students.oamk.fi>
2026-03-03 22:57:33 +0200

    Async Storage fix 2

--- pages/LoginPage.js
+++ pages/LoginPage.js
@@ -33,10 +33,9 @@ export default function LoginPage(){
 
                 //console.log("Login Successfull!");
             } else {
-                
                 await createUserWithEmailAndPassword(auth,email,password);
                 // console.log("Registration Successfull!");
-                await AsyncStorage.setItem('username', user.email);
+                await AsyncStorage.setItem('username', email);
                 Alert.alert("Success", "Account created!");
             }
 
